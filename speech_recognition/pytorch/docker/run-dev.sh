37107b7b29873a0a28502423eec34f401d782486
Jacob <jiahuanglin27@gmail.com>
2018-10-07 15:30:52 -0400

    modify run-dev path

--- speech_recognition/pytorch/docker/run-dev.sh
+++ speech_recognition/pytorch/docker/run-dev.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 nvidia-docker run \
-  -v /mnt/disk/mnt_dir:/mnt/disk/mnt_dir:rw \
+  -v /home/jacob/github/training/speech_recognition:/home/jacob/github/training/speech_recognition:rw \
   -v /etc/passwd:/etc/passwd:ro \
   -it --rm --user $(id -u) ds2-cuda9cudnn7:gpu
+  
