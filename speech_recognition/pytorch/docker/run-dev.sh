8fd6b902d07b2f85a5141bdc3abd97d149633d42
Jiahuang Lin <jacob@mel-18.syslab.sandbox>
2018-10-13 00:42:14 -0400

    modified scripts

--- speech_recognition/pytorch/docker/run-dev.sh
+++ speech_recognition/pytorch/docker/run-dev.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 nvidia-docker run \
   -v /home/jacob/github/training/speech_recognition:/home/jacob/github/training/speech_recognition:rw \
+  -v /scratch:/scratch:rw \
   -v /etc/passwd:/etc/passwd:ro \
-  -it --rm --user $(id -u) ds2-cuda9cudnn7:gpu
-  
+  -it --rm -u 0 ds2-cuda9cudnn7:gpu
