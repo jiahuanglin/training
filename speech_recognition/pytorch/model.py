393c4d83d3dfeff705ff6167b10f58ae41b1c43c
Jacob <jiahuanglin27@gmail.com>
2018-10-14 01:07:08 -0400

    torchV2 not working, revert to previous

--- speech_recognition/pytorch/model.py
+++ speech_recognition/pytorch/model.py
@@ -101,7 +101,7 @@ class BatchRNN(nn.Module):
 
         if self.bidirectional:
             x = x.view(x.size(0), x.size(1), 2, -1).sum(2).view(x.size(0), x.size(1), -1)  # (TxNxH*2) -> (TxNxH) by sum
-        self.rnn.flatten_parameters()
+        # self.rnn.flatten_parameters()
         return x
 
 
