669d241d43ef9ddd1c50e0af26cfc022f07c7a8d
ahsueh1996 <ahsueh1996@gmail.com>
2018-10-22 19:07:59 -0400

    run-dev auto lock to current user folder and fix_torchaudio.sh script to quickly reinstall troch audio without reverting to old commit

--- speech_recognition/pytorch/docker/run-dev.sh
+++ speech_recognition/pytorch/docker/run-dev.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
+
 nvidia-docker run \
   --shm-size 64G \
-  -v /home/jacob/github/training/speech_recognition:/home/jacob/github/training/speech_recognition:rw \
+  -v /home/$USER/github/training/speech_recognition:/home/jacob/github/training/speech_recognition:rw \
   -v /scratch:/scratch:rw \
   -v /etc/passwd:/etc/passwd:ro \
   -it --rm -u 0 ds2-cuda9cudnn7:gpu
