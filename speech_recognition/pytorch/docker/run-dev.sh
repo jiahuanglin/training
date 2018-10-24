8cfd96a774b210a9931f006e54ea62a8bef2713a
ahsueh1996 <ahsueh1996@gmail.com>
2018-10-24 13:12:31 +0000

    expose port 5050 in docker for dist train

--- speech_recognition/pytorch/docker/run-dev.sh
+++ speech_recognition/pytorch/docker/run-dev.sh
@@ -5,6 +5,7 @@ nvidia-docker run \
   -v /home/$USER/github/training/speech_recognition:/home/jacob/github/training/speech_recognition:rw \
   -v /scratch:/scratch:rw \
   -v /etc/passwd:/etc/passwd:ro \
+  -p 127.0.0.1:5050:5050/tcp \
   -it --rm -u 0 ds2-cuda9cudnn7:gpu
 
 
