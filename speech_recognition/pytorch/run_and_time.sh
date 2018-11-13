4855af66f5fea1e1d7bac895d24abc5bfd39cb3c
Jacob <jiahuanglin27@gmail.com>
2018-11-13 06:22:40 +0000

    fix syntax bugs

--- speech_recognition/pytorch/run_and_time.sh
+++ speech_recognition/pytorch/run_and_time.sh
@@ -3,6 +3,6 @@
 RANDOM_SEED=1
 TARGET_ACC=23
 
-python dist_train.py --world_size=2 --checkpoint --model_path models/deepspeech_t$RANDOM_SEED.pth.tar --seed $RANDOM_SEED --acc $TARGET_ACC
-#python train.py --checkpoint --model_path models/deepspeech_t$RANDOM_SEED.pth.tar --seed $RANDOM_SEED --acc $TARGET_ACC
+#python dist_train.py --world_size=2 --checkpoint --model_path models/deepspeech_t$RANDOM_SEED.pth.tar --seed $RANDOM_SEED --acc $TARGET_ACC
+python train.py --checkpoint --model_path models/deepspeech_t$RANDOM_SEED.pth.tar --seed $RANDOM_SEED --acc $TARGET_ACC
 #python train.py --checkpoint --continue_from models/deepspeech_${1}.pth.tar --model_path models/deepspeech_t$RANDOM_SEED.pth.tar --seed $RANDOM_SEED --acc $TARGET_ACC
